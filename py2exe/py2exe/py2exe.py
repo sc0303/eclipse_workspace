616ae8db14328e79f83c8d8e818ceaedb3b6a332
SunChuan <sunchuan@bestpay.com.cn>
2014-07-07 09:26:36 +0800

    commit on 0707

---
+++ py2exe/py2exe/py2exe.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-  
+#coding=utf-8  
+'''
+Created on 2014年7月3日
+
+@author: Alance
+'''
+from  distutils.core  import  setup  
+import  py2exe  
+if __name__ == '__main__':
+    setup(windows=[{"script" :  "wxpython.py" }]) 
---
+++ wxpython/wxpython/wxpython.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-  
+#coding=utf-8  
+'''
+Created on 2014年7月3日
+
+@author: Alance
+'''
+import wx
+if __name__ == '__main__':
+    app = wx.PySimpleApp()  
+    frame = wx.Frame(parent=None )  
+    frame.Show(True )  
+    app.MainLoop()
